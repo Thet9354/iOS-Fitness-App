db89cc4a5824d1634b37074852bac60c09cecf32
Thet Pine <thetpine254@gmail.com>
2025-03-27 18:56:40 +0800

    LeaderboardView UI

---
+++ Fitness App/LeaderBoard/Views/LeaderboardView.swift
@@ -0,0 +1,18 @@
+//
+//  LeaderboardView.swift
+//  Fitness App
+//
+//  Created by Phoon Thet Pine on 26/3/25.
+//
+
+import SwiftUI
+
+struct LeaderboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LeaderboardView()
+}
---
+++ Fitness App/LeaderBoard/Views/TermsView.swift
@@ -0,0 +1,18 @@
+//
+//  TermsView.swift
+//  Fitness App
+//
+//  Created by Phoon Thet Pine on 26/3/25.
+//
+
+import SwiftUI
+
+struct TermsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TermsView()
+}
